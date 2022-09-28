8b8c83e90cb6db2a601fd69b42ea7eb12066aa3e
In-Gvarr <112261300+In-Gvarr@users.noreply.github.com>
2022-09-28 21:44:24 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        long feliksLive = 3_011_456_567L;
+        long answer = (feliksLive / 31_622_400);
+
+        System.out.println(answer);
+
+
+    }
+}
